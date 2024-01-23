306143d4dcd59a6357482b7d49a53316597b1086
L0U-AN <146362124+L0U-AN@users.noreply.github.com>
2024-01-23 09:06:28 +0100

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.airsoftApp123',
+  appName: 'Airsoft App',
+  webDir: 'www',
+  bundledWebRuntime: false,
+};
+
+export default config;
