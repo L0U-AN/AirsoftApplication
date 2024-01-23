fca29ac8dd1770f165e4f198d2bebaf23f5865c8
L0U-AN <146362124+L0U-AN@users.noreply.github.com>
2024-01-23 09:15:31 +0100

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'ba-eventplanner',
+    appId: '1:427112425330:web:1ce42bb972d89559779feb',
+    storageBucket: 'ba-eventplanner.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyD9_HIrkCcS7bvDkNXTeIGy7u3Udn990o8',
+    authDomain: 'ba-eventplanner.firebaseapp.com',
+    messagingSenderId: '427112425330',
+    measurementId: 'G-H6SGG4JE5Q',
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,46 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyBoLB1K-Oq25WT4JXibDrPlQR8F1J-fPr4',
+
+  authDomain: 'airsoftapplication-19aa6.firebaseapp.com',
+
+  projectId: 'airsoftapplication-19aa6',
+
+  storageBucket: 'airsoftapplication-19aa6.appspot.com',
+
+  messagingSenderId: '667494229395',
+
+  appId: '1:667494229395:web:196e70ed64b431df66b466'
+
+  },
+  production: false,
+  mapsKey:'AIzaSyCKQt9vnxQ_mWMRuwBdfnnRcn3mXguO8L0',
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig: {
+    apiKey: 'AIzaSyBoLB1K-Oq25WT4JXibDrPlQR8F1J-fPr4',
+
+  authDomain: 'airsoftapplication-19aa6.firebaseapp.com',
+
+  projectId: 'airsoftapplication-19aa6',
+
+  storageBucket: 'airsoftapplication-19aa6.appspot.com',
+
+  messagingSenderId: '667494229395',
+
+  appId: '1:667494229395:web:196e70ed64b431df66b466'
+
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
